1e562caa262825add6e2d79eff93d479c90be1ff
lgp00 <askmyinprivate@askme.me>
2025-01-14 10:37:44 +0200

    update favicon path and modify page title in index.html; adjust logo links in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,9 @@
 
 <template>
 	<div>
-		<a href="https://vite.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		<img src="/vite.svg" class="logo" alt="Vite logo" />
+
+		<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
 	</div>
 	<HelloWorld msg="Vite + Vue" />
 </template>
